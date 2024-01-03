9358c80263a3469444e4b58e967af8e95256a280
april211 <wuguangkai2018@outlook.com>
2024-01-03 20:38:56 +0800

    write comments

--- src/utils/debug_utils.py
+++ src/utils/debug_utils.py
@@ -1,8 +1,15 @@
-import torch as th
+import torch
 import numpy as np
 
 
-def topk_uniques(adj, k=5):
-    topk_neighbors = th.topk(adj, k).indices.flatten().cpu().numpy().tolist()
-    d = {v: sum(topk_neighbors == v) / adj.shape[0] for v in np.unique(topk_neighbors)}
-    return {k: round(d[k], 4) for k in sorted(d, key=d.get, reverse=True)}
+def topk_uniques(sim, k=5) -> dict:
+    """
+    Return the most common nodes in the top-k neighbors of each node \
+        and their "degree" (i.e., the number of times they appear in the top-k neighbors of all nodes).
+    """
+    topk_neighbors = torch.topk(sim, k).indices.flatten().cpu().numpy().tolist()
+
+    # sum(topk_neighbors == v): "degree" of node v.
+    std_degree_dict = {v: sum(topk_neighbors == v) / sim.shape[0] for v in np.unique(topk_neighbors)}
+    return {v: round(std_degree_dict[v], 4) for v in sorted(std_degree_dict, key=std_degree_dict.get, reverse=True)}
+
