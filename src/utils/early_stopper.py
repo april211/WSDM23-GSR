a7af98bff6c4257848ddc7bb8e8741b9aa9293df
april211 <wuguangkai2018@outlook.com>
2024-01-08 10:16:21 +0800

    write comments, clean up imports

--- src/utils/early_stopper.py
+++ src/utils/early_stopper.py
@@ -1,20 +1,22 @@
 """
-Early stop provided by DGL
+Early stopping provided by DGL.
 """
-import numpy as np
 import torch
 
 
 class EarlyStopping:
-    def __init__(self, patience=10, path='es_checkpoint.pt'):
+    def __init__(self, patience=10, ckpoint_path='es_checkpoint.pt'):
         self.patience = patience
         self.counter = 0
         self.best_score = None
         self.best_epoch = None
-        self.early_stop = False
-        self.path = path
+        self.should_stop = False
+        self.ckpoint_path = ckpoint_path
 
     def step(self, acc, model, epoch):
+        """
+        Return True if should stop training (exceed patience).
+        """
         score = acc
         if self.best_score is None:
             self.best_score = score
@@ -25,15 +27,16 @@ class EarlyStopping:
             print(
                 f'EarlyStopping counter: {self.counter}/{self.patience}, best_val_score:{self.best_score:.4f} at E{self.best_epoch}')
             if self.counter >= self.patience:
-                self.early_stop = True
+                self.should_stop = True
         else:
             self.best_score = score
             self.best_epoch = epoch
             self.save_checkpoint(model)
             self.counter = 0
 
-        return self.early_stop
+        return self.should_stop
 
     def save_checkpoint(self, model):
         '''Saves model when validation loss decrease.'''
-        torch.save(model.state_dict(), self.path)
+        torch.save(model.state_dict(), self.ckpoint_path)
+
